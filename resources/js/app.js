e9cdeab2ccd5919de5eecad3009ac6f001fab9b3
Gustavo Pu <gpu@cari.net>
2023-03-25 10:07:51 -0600

    config detail

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 import Alpine from 'alpinejs'
+import AlpineFloatingUI from '@awcodes/alpine-floating-ui'
 import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
 import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
 
+Alpine.plugin(AlpineFloatingUI)
 Alpine.plugin(FormsAlpinePlugin)
 Alpine.plugin(NotificationsAlpinePlugin)
+Alpine.plugin(focus)
+
 
 window.Alpine = Alpine
 
